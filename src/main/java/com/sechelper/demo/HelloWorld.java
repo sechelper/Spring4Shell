96af8ab3a3ac14231438c74690945a30bfb22fb5
sechelper <100655242+sechelper@users.noreply.github.com>
2022-04-03 21:16:40 +0800

    Update HelloWorld.java

--- src/main/java/com/sechelper/demo/HelloWorld.java
+++ src/main/java/com/sechelper/demo/HelloWorld.java
@@ -1,16 +1,13 @@
 package com.sechelper.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-    
-    @PostMapping("/hello")
-    @GetMapping("/hello")
+
+    @RequestMapping("/hello")
     private String hello(User user){
         return "hello " + user.getName();
     }
-    
 }
